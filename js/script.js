f1cd0fb9fbf2a024a1ce85f6ad4bdf47e9136363
Sergey <serezha.ros@mail.ru>
2020-06-11 22:10:27 +0300

    adding a timer

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
- window.addEventListener('DOMContentLoaded', function() {
-    
+window.addEventListener('DOMContentLoaded', function () {
+
     'use strict';
     let tab = document.querySelectorAll('.info-header-tab');
     let info = document.querySelector('.info-header');
@@ -21,10 +21,10 @@
         }
     }
 
-    info.addEventListener('click', function(event) {
+    info.addEventListener('click', function (event) {
         let target = event.target;
         if (target && target.classList.contains('info-header-tab')) {
-            for(let i = 0; i < tab.length; i++) {
+            for (let i = 0; i < tab.length; i++) {
                 if (target == tab[i]) {
                     hideTabContent(0);
                     showTabContent(i);
@@ -33,4 +33,43 @@
             }
         }
     });
+
+    // Timer
+
+    let deadline = '2020-06-11';
+
+    function getTimeRemaining(endtime) {
+        let t = Date.parse(endtime) - Date.parse(new Date());
+        let seconds = Math.floor((t / 1000) % 60);
+        let minutes = Math.floor((t / 1000 / 60) % 60);
+        let hours = Math.floor((t / (1000 * 60 * 60)));
+
+        return {
+            'total': t,
+            'hours': hours,
+            'minutes': minutes,
+            'seconds': seconds,
+        };
+    }
+
+    function setClock(id, endtime) {
+        let timer = document.getElementById(id);
+        let hours = timer.querySelector('.hours');
+        let minutes = timer.querySelector('.minutes');
+        let seconds = timer.querySelector('.seconds');
+        let timeInterval = setInterval(updateClock, 1000);
+
+        function updateClock() {
+            let t = getTimeRemaining(endtime);
+            hours.textContent = t.hours;
+            minutes.textContent = t.minutes;
+            seconds.textContent = t.seconds;
+
+            if (t.total <= 0) {
+                clearInterval(timeInterval);
+            }
+        }
+    }
+
+    setClock('timer', deadline);
 });
